80fcb90ab3177f9ac82a95e8fa17131e98db14d3
atulstpt <atulstpt@hotmail.com>
2025-07-17 19:10:52 +0530

    Remove unnecessary blank lines in BirthdayEmailProjectApplication.java

--- src/main/java/com/agstechcloud/birthdayjob/BirthdayEmailProjectApplication.java
+++ src/main/java/com/agstechcloud/birthdayjob/BirthdayEmailProjectApplication.java
@@ -7,9 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class BirthdayEmailProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BirthdayEmailProjectApplication.class, args);
 	}
-
 }
